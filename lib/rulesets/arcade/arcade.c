9a451f9620a8c13917e7256be2ad44f178c4dcb5
Nathan Du <nathandu@outlook.com>
2023-11-07 17:46:55 +0800

    move arcade ruleset definitions to private include

--- lib/rulesets/arcade/arcade.c
+++ lib/rulesets/arcade/arcade.c
@@ -20,7 +20,7 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-#include "arcade.h"
+#include "rulesets/arcade.h"
 #include <config.h>
 #include <quadus.h>
 #include <quadus/calls.h>
--- lib/rulesets/arcade/pieces.c
+++ lib/rulesets/arcade/pieces.c
@@ -20,7 +20,7 @@
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
  * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
-#include "arcade.h"
+#include "rulesets/arcade.h"
 #include <quadus.h>
 #include <quadus/piece.h>
 
